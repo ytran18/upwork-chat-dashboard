96097e56bd1c73fadafe90b7e65151d17823a88c
jin <a11718trannhuy@gmail.com>
2024-01-22 08:02:36 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,164 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import * as React from 'react';
 
-const inter = Inter({ subsets: ["latin"] });
+import { TooltipProvider } from "@/utils/ui/tooltip";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/utils/ui/resizable';
+import { AccountSwitcher } from '@/utils/components/account-switcher';
+import { Separator } from '@/utils/ui/separator';
+import { Nav } from '@/utils/components/nav';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/utils/ui/tabs';
+import { Input } from '@/utils/ui/input';
+import { MailList } from '@/utils/components/mail-list';
+import { MailDisplay } from '@/utils/components/mail-display';
+import { useMail } from '@/utils/use-mail';
+
+import { accounts, mails } from '@/utils/data';
+
+import { AlertCircle, Archive, ArchiveX, File, Inbox, MessagesSquare, PenBox, Search, Send, ShoppingCart, Trash2, Users2 } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const [isCollapsed, setIsCollapsed] = React.useState(false)
+    const [mail] = useMail()
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <TooltipProvider delayDuration={0}>
+            <ResizablePanelGroup
+                direction="horizontal"
+                onLayout={(sizes: number[]) => {
+                    document.cookie = `react-resizable-panels:layout=${JSON.stringify(sizes)}`
+                }}
+                className="h-screen max-h-[800px] items-stretch"
+            >
+                <ResizablePanel
+                    defaultSize={265}
+                    collapsedSize={4}
+                    collapsible={true}
+                    minSize={15}
+                    maxSize={20}
+                    // onCollapse={(collapsed) => {
+                    //     setIsCollapsed(collapsed)
+                    //     document.cookie = `react-resizable-panels:collapsed=${JSON.stringify(collapsed)}`
+                    // }}
+                    className={cn(isCollapsed && "min-w-[50px] transition-all duration-300 ease-in-out")}
+                >
+                    <div className={cn("flex h-[52px] w-full items-center justify-center", isCollapsed ? 'h-[52px]': 'px-2')}>
+                        <AccountSwitcher isCollapsed={isCollapsed} accounts={accounts} />
+                    </div>
+                    <Separator />
+                    <Nav
+                        isCollapsed={isCollapsed}
+                        links={[
+                            {
+                                title: "Inbox",
+                                label: "128",
+                                icon: Inbox,
+                                variant: "default",
+                            },
+                            {
+                                title: "Drafts",
+                                label: "9",
+                                icon: File,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Sent",
+                                label: "",
+                                icon: Send,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Junk",
+                                label: "23",
+                                icon: ArchiveX,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Trash",
+                                label: "",
+                                icon: Trash2,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Archive",
+                                label: "",
+                                icon: Archive,
+                                variant: "ghost",
+                            },
+                        ]}
+                    />
+                    <Separator />
+                    <Nav
+                        isCollapsed={isCollapsed}
+                        links={[
+                            {
+                                title: "Social",
+                                label: "972",
+                                icon: Users2,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Updates",
+                                label: "342",
+                                icon: AlertCircle,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Forums",
+                                label: "128",
+                                icon: MessagesSquare,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Shopping",
+                                label: "8",
+                                icon: ShoppingCart,
+                                variant: "ghost",
+                            },
+                            {
+                                title: "Promotions",
+                                label: "21",
+                                icon: Archive,
+                                variant: "ghost",
+                            },
+                        ]}
+                    />
+                </ResizablePanel>
+                <ResizableHandle withHandle/>
+                <ResizablePanel defaultSize={440} minSize={30}>
+                    <Tabs defaultValue='all'>
+                        <div className="flex items-center px-4 py-2">
+                            <h1 className="text-xl font-bold">Inbox</h1>
+                            <TabsList className="ml-auto">
+                                <TabsTrigger value="all" className="text-zinc-600 dark:text-zinc-200">All mail</TabsTrigger>
+                                <TabsTrigger value="unread" className="text-zinc-600 dark:text-zinc-200">Unread</TabsTrigger>
+                            </TabsList>
+                        </div>
+                        <Separator />
+                        <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                            <form>
+                                <div className="relative">
+                                    <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                                    <Input placeholder="Search" className="pl-8" />
+                                </div>
+                            </form>
+                        </div>
+                        <TabsContent value='all' className='m-0'>
+                            <MailList items={mails}/>
+                        </TabsContent>
+                        <TabsContent value="unread" className="m-0">
+                            <MailList items={mails.filter((item) => !item.read)} />
+                        </TabsContent>
+                    </Tabs>
+                </ResizablePanel>
+                <ResizableHandle withHandle/>
+                    <ResizablePanel defaultSize={655}>
+                        <MailDisplay
+                            mail={mails.find((item) => item.id === mail.selected) || null}
+                        />
+                    </ResizablePanel>
+            </ResizablePanelGroup>
+        </TooltipProvider>
+    );
 }
